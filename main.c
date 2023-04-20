1121ba9de2284c2d6dac69468e3415f8b88c2c28
Kraiwit Samanpiew <kraiwit.chokunsr@gmail.com>
2023-04-20 11:35:18 +0700

    c exponent

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double base, exponent, result;
+
+    /* base */
+    printf("base: ");
+    scanf("%lf", &base);
+
+    /* exponent */
+    printf("exponent: ");
+    scanf("%lf", &exponent);
+
+    result = pow(base, exponent);
+    printf("result is %2.f\n", result);
+}
